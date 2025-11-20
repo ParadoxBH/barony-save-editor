4c4f8150b21b78e120b4a6fe3ce2f7243715af5a
Guilherme Garcia <99338371+ParadoxBH@users.noreply.github.com>
2025-11-20 19:57:30 -0300

    TabWindow: Melhoria diversas no componente destinadas a personalização

--- src/components/TabWindow.tsx
+++ src/components/TabWindow.tsx
@@ -3,18 +3,47 @@ import { type ReactNode } from "react";
 
 interface TabWindowProps {
   label: string;
-  children: ReactNode;
+  children?: ReactNode;
   width?: number;
+  actions?: ReactNode;
+  prefix?: ReactNode;
+  postfix?: ReactNode;
+  startIcon?: ReactNode;
 }
 
-export function TabWindow({ label, children, width }: TabWindowProps) {
+export function TabWindow({
+  label,
+  children,
+  width,
+  actions,
+  prefix,
+  postfix,
+  startIcon,
+}: TabWindowProps) {
   return (
     <Box maxHeight={"100%"} display={"flex"}>
-      <Paper elevation={3} sx={{ display: "flex"}}>
+      <Paper elevation={3} sx={{ display: "flex" }}>
         <Stack alignItems={"center"} spacing={2} p={2}>
-          <Typography variant="h6" gutterBottom>
-            {label}
-          </Typography>
+          <Stack
+            direction={"row"}
+            spacing={4}
+            alignItems={"center"}
+            justifyContent={actions ? "space-between" : "center"}
+            width={"100%"}
+          >
+            <Stack direction={"row"} spacing={2} alignItems={"center"}>
+              {startIcon}
+              <Typography variant="h6" gutterBottom>
+                {label}
+              </Typography>
+            </Stack>
+            {actions && (
+              <Stack direction={"row"} spacing={0.5} alignItems={"center"}>
+                {actions}
+              </Stack>
+            )}
+          </Stack>
+          {prefix}
           <Box
             flex={1}
             width={width}
@@ -22,6 +51,7 @@ export function TabWindow({ label, children, width }: TabWindowProps) {
           >
             {children}
           </Box>
+          {postfix}
         </Stack>
       </Paper>
     </Box>
